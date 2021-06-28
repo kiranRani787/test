e8e144e388d138a8c990f356e811f5bb851a87fa
kiran.rani <rani.kiran787@gmail.com>
2021-06-28 22:55:20 +0530

    first commit

---
+++ fileMovementProcess/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+namespace fileMovementProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string filePickUp = ConfigurationManager.AppSettings["filePickUp"];
+                string fileDropoff = ConfigurationManager.AppSettings["fileDropoff"];
+                
+                List<String> MyFiles = Directory
+                       .GetFiles(filePickUp, "*.*").ToList();
+
+                foreach (string file in MyFiles)
+                {
+                    FileInfo mFile = new FileInfo(file);
+                    // to remove name collisions
+                    if (mFile.Name.Contains("Print") == true)
+                    {
+                        mFile.MoveTo(fileDropoff + "\\" + mFile.Name);
+                        Console.WriteLine("File transferred");
+                    }
+
+                    }
+                
+
+               
+            }
+        }
+    }
+}
